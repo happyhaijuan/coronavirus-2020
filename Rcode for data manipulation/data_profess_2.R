7b8139237b2044923e22eb9208cf11c1eab21e6f
haijuan <haijuan.iastate.edu>
2020-02-15 12:42:31 -0600

    time series group

--- Rcode for data manipulation/data_profess_2.R
+++ Rcode for data manipulation/data_profess_2.R
@@ -12,3 +12,8 @@ names(merged_df)[names(merged_df) == 'cases.y'] <- 'death_number'
 names(merged_df)[names(merged_df) == 'cases'] <- 'recoved_number'
 merged_df <- merged_df[,c(1,2,3,4,5,7,9)]
 write.csv(merged_df,'/Users/haijuanzhang/Documents/coronavirus-2020/visulization data/total_numbers.csv')
+raw_data$date <- as.Date(raw_data$date)
+time_series <- raw_data %>% select(date,cases,type) %>% group_by(date,type) %>% summarise(total_case = sum(cases))
+ggplot(time_series,aes(x = date, y = total_case, group = type)) + geom_line(aes(linetype = type, color = type)) +
+  geom_point(aes(color=type)) + theme(legend.position = "bottom",plot.margin = margin(0.1,0.1,0.1,0.1, "cm")) 
+write.csv(time_series,'/Users/haijuanzhang/Documents/coronavirus-2020/visulization data/time_series_group.csv')
