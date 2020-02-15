a5d364d088ea2ef24e16e26fdbe33bdf9e0c59cd
haijuan <haijuan.iastate.edu>
2020-02-15 12:41:07 -0600

    modify

--- Rshiny/server.R
+++ Rshiny/server.R
@@ -2,12 +2,18 @@ library(shiny)
 library(dplyr)
 library(leaflet)
 library(DT)
-
+library(ggplot2)
 shinyServer(function(input,output){
   # Import data
-  df_coron <- read.csv("/Users/haijuanzhang/Documents/coronavirus-2020/visulization data/total_numbers.csv",
+  df_coron <- read.csv("total_numbers.csv",
                       stringsAsFactors = FALSE)
+  coronavirus <- read.csv("coronavirus.csv",
+                      stringsAsFactors = FALSE)
+  time_series_group <- read.csv("time_series_group.csv",
+                                stringsAsFactors = FALSE)
+  
   df_coron <- data.frame(df_coron)
+  coronavirus <- data.frame(coronavirus)
  # column for label
  df_coron <- mutate(df_coron, cntnt=paste0('<strong>Name:</strong>',Country.Region,
                                           '<br><strong>State/Provice:</strong>',Province.State,
@@ -30,12 +36,15 @@ shinyServer(function(input,output){
        # addLegend(pal=pal,values=df_coron$type, opacity = 1, na.label = "Not Available") 
         addEasyButton(easyButton(icon="fa-crosshairs",title="ME",onClick = JS("function(btn,map){map.locate({setView:true});}")))
     #create a data object to display data
-    
-   # output$data <-DT::renderDataTable(datatable(
-   #   df_coron,filter = 'top',
-  #   colnames = c("Province.State", "Country.Region", "Lat","Long","date","case","type")
-   # ))
-                          
   })
+  output$plot <- renderPlot({ggplot(time_series_group,aes(x = date, y = total_case, group = type)) + geom_line(aes(linetype = type, color = type)) +
+      geom_point(aes(color=type)) + theme(legend.position = "bottom",plot.margin = margin(0.1,0.1,0.1,0.1, "cm"))})
+  
+  output$data <-DT::renderDataTable(datatable(
+      coronavirus,filter = 'top',
+     colnames = c("Row","Province.State", "Country.Region", "Lat","Long","date","cases","type")
+    ))
+                          
+ 
 })
 
--- Rshiny/ui.R
+++ Rshiny/ui.R
@@ -12,8 +12,11 @@ library(leaflet)
 library(spData)
 library(dplyr)
 
+#selectInput(inputId = "Select",label = "select a country",choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3))
+
 navbarPage("Information about Coronavirus in 2020", id="main",
-           tabPanel("Map", leafletOutput("bbmap", height=1000))
-          # tabPanel("Data", DT::dataTableOutput("data"))
+           tabPanel("Map", leafletOutput("bbmap", height=1000)),
+           tabPanel("Trend",plotOutput("plot")),
+           tabPanel("Data", DT::dataTableOutput("data"))
           # tabPanel("Read Me",includeMarkdown("readme.md"))
           )
